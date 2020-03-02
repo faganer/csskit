c2b7fbad01d9f591c1db2546a8ad0152903631de
faganer <xnfaganer@163.com>
2020-03-02 23:22:22 +0800

    更新盒子模型，更新输入、文本框样式

--- gulpfile.js
+++ gulpfile.js
@@ -30,39 +30,7 @@ function clean() {
 function styles() {
     var plugins = [
         autoprefixer(),
-        // cssnano({
-        //     // 预设
-        //     "preset": [
-        //         "default",
-        //         // 删除所有注释
-        //         { "discardComments": { "removeAll": true } }
-        //     ]
-        // })
-        // cssnano({ "discardComments": { "removeAll": true } })
-    ];
-    return gulp.src(paths.styles.src)
-        .pipe(changed(paths.styles.dest))
-        .pipe(sass())
-        .pipe(postcss(plugins))
-        // pass in options to the stream
-        // .pipe(rename({
-        //     // basename: 'style',
-        //     suffix: '.min'
-        // }))
-        .pipe(gulp.dest(paths.styles.dest));
-}
-
-function stylesMin() {
-    var plugins = [
-        autoprefixer(),
-        // cssnano({
-        //     // 预设
-        //     "preset": [
-        //         "default",
-        //         // 删除所有注释
-        //         { "discardComments": { "removeAll": true } }
-        //     ]
-        // })
+        // 删除所有注释
         cssnano({ "discardComments": { "removeAll": true } })
     ];
     return gulp.src(paths.styles.src)
@@ -81,15 +49,11 @@ function watch() {
     gulp.watch(paths.styles.src, styles);
 }
 
-function build() {
-    gulp.watch(paths.styles.src, stylesMin);
-}
-
 /*
  * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
  */
 // var build = gulp.series(clean, gulp.parallel(styles, scripts, images, sprite, ajax, fonts, watch));
-// var build = gulp.series(styles);
+var build = gulp.series(styles);
 
 /*
  * You can use CommonJS `exports` module notation to declare tasks
