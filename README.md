bf8df26cc0a54cebd3905a91ae686c3f9a50e27a
faganer <xnfaganer@163.com>
2020-03-02 17:49:56 +0800

    提交基础

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # csskit
 简单的CSS组件库。
+
+## Base
+基于normalize.css v8.0.1。
+
+## Flex
+基于UIkit 3.3.3。
+
+## Grids
+基于PureCSS 1.0.1。
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var changed = require('gulp-changed');
+var del = require('del');
+
+var paths = {
+    styles: {
+        src: 'src/sass/**/*.scss',
+        dest: 'dist/css/'
+    }
+};
+
+/* Not all tasks need to use streams, a gulpfile is just another node program
+ * and you can use all packages available on npm, but it must return either a
+ * Promise, a Stream or take a callback and call it
+ */
+function clean() {
+    // You can use multiple globbing patterns as you would with `gulp.src`,
+    // for example if you are using del 2.0 or above, return its promise
+    return del(['dist']);
+}
+
+/*
+ * Define our tasks using plain functions
+ */
+function styles() {
+    var plugins = [
+        autoprefixer(),
+        // cssnano({
+        //     // 预设
+        //     "preset": [
+        //         "default",
+        //         // 删除所有注释
+        //         { "discardComments": { "removeAll": true } }
+        //     ]
+        // })
+        cssnano({ "discardComments": { "removeAll": true } })
+    ];
+    return gulp.src(paths.styles.src)
+        .pipe(changed(paths.styles.dest))
+        .pipe(sass())
+        .pipe(postcss(plugins))
+        // .pipe(cleanCSS())
+        // pass in options to the stream
+        .pipe(rename({
+            // basename: 'style',
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+}
+
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+// var build = gulp.series(clean, gulp.parallel(styles, scripts, images, sprite, ajax, fonts, watch));
+var build = gulp.series(styles);
+
+/*
+ * You can use CommonJS `exports` module notation to declare tasks
+ */
+exports.clean = clean;
+exports.styles = styles;
+exports.watch = watch;
+exports.build = build;
+
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+exports.default = build;
