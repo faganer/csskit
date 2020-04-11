53b90f3d6320f0c8af214c3414a5af1c759f14bd
faganer <xnfaganer@163.com>
2020-04-11 12:06:55 +0800

    gulp自动watch

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,14 @@ var changed = require('gulp-changed');
 var del = require('del');
 
 var paths = {
-  styles: {
-    src: 'src/sass/**/*.scss',
-    dest: 'dist/css/'
-  },
-  docStyle: {
-    src: 'doc/**/*.scss',
-    dest: 'doc/'
-  }
+    styles: {
+        src: 'src/sass/**/*.scss',
+        dest: 'dist/css/'
+    },
+    docStyle: {
+        src: 'doc/**/*.scss',
+        dest: 'doc/'
+    }
 };
 
 /* Not all tasks need to use streams, a gulpfile is just another node program
@@ -23,62 +23,62 @@ var paths = {
  * Promise, a Stream or take a callback and call it
  */
 function clean() {
-  // You can use multiple globbing patterns as you would with `gulp.src`,
-  // for example if you are using del 2.0 or above, return its promise
-  return del(['dist']);
+    // You can use multiple globbing patterns as you would with `gulp.src`,
+    // for example if you are using del 2.0 or above, return its promise
+    return del(['dist']);
 }
 
 /*
  * Define our tasks using plain functions
  */
 function styles() {
-  var plugins = [
-    autoprefixer(),
-    // 删除所有注释
-    cssnano({ "discardComments": { "removeAll": true } })
-  ];
-  return gulp.src(paths.styles.src)
-    .pipe(changed(paths.styles.dest))
-    .pipe(sass())
-    .pipe(postcss(plugins))
-    // pass in options to the stream
-    .pipe(rename({
-      // basename: 'style',
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(paths.styles.dest));
+    var plugins = [
+        autoprefixer(),
+        // 删除所有注释
+        cssnano({ "discardComments": { "removeAll": true } })
+    ];
+    return gulp.src(paths.styles.src)
+        .pipe(changed(paths.styles.dest))
+        .pipe(sass())
+        .pipe(postcss(plugins))
+        // pass in options to the stream
+        .pipe(rename({
+            // basename: 'style',
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(paths.styles.dest));
 }
 
 /*
  * Doc
  */
 function docStyle() {
-  var plugin = [
-    autoprefixer(),
-    // 删除所有注释
-    cssnano({ "discardComments": { "removeAll": true } })
-  ];
-  return gulp.src(paths.docStyle.src)
-    // .pipe(changed(paths.docStyle.dest))
-    .pipe(sass())
-    .pipe(postcss(plugin))
-    // pass in options to the stream
-    .pipe(rename({
-      // basename: 'style',
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(paths.docStyle.dest));
+    var plugin = [
+        autoprefixer(),
+        // 删除所有注释
+        cssnano({ "discardComments": { "removeAll": true } })
+    ];
+    return gulp.src(paths.docStyle.src)
+        // .pipe(changed(paths.docStyle.dest))
+        .pipe(sass())
+        .pipe(postcss(plugin))
+        // pass in options to the stream
+        .pipe(rename({
+            // basename: 'style',
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(paths.docStyle.dest));
 }
 
 function watch() {
-  gulp.watch(paths.styles.src, styles);
-  gulp.watch(paths.docStyle.src, docStyle);
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.docStyle.src, docStyle);
 }
 
 /*
  * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
  */
-var build = gulp.series(gulp.parallel(styles, docStyle));
+var build = gulp.series(gulp.parallel(styles, docStyle, watch));
 
 /*
  * You can use CommonJS `exports` module notation to declare tasks
