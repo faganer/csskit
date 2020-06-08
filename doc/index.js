7b632d2f20bbb1f0921adb6731b3b7bf17652265
谢德旭 <xiedx@szkingdom.com>
2020-06-08 18:26:45 +0800

    更新table

--- doc/index.js
+++ doc/index.js
@@ -1,29 +1,34 @@
-$(function() {
-    hljs.initHighlightingOnLoad();
-    tabs(".sidebar li", ".component-item", '8');
-    // dialog
-    $(".component-dialog .demo-content-item:first p a").click(function() {
-        var type = $(this).text();
-        dialog({
-            type: type,
-            content: '这是一个 <b>' + type + '</b> 弹窗。',
-        })
-    });
+$(function () {
 
-    // modal
-    $(".modal-event").click(function() {
-        modal(".modal", "show");
-        console.log("modal")
-    });
-    $(".modal .btn-primary").click(function() {
-        modal(".modal", "hide");
-    });
+  // hightlinght.js
+  hljs.initHighlightingOnLoad();
 
-    // tab
-    tabs(".tab-nav", ".tab-content-item", '1');
-    var demoContainer = ['component-background', 'component-buttons', 'component-cover', 'component-dialog', 'component-flex .component-demo:first', 'component-flex .component-demo:eq(1)', 'component-flex .component-demo:eq(2)'];
-    demoContainer.map(item => {
-        tabs("." + item + " .demo-nav-item", "." + item + " .demo-content-item", '1');
-    });
+  // layout
+  tabs(".sidebar li", ".component-item", '10');
+
+  // dialog
+  $(".component-dialog .demo-content-item:first p a").click(function () {
+    var type = $(this).text();
+    dialog({
+      type: type,
+      content: '这是一个 <b>' + type + '</b> 弹窗。',
+    })
+  });
+
+  // modal
+  $(".modal-event").click(function () {
+    modal(".modal", "show");
+    console.log("modal")
+  });
+  $(".modal .btn-primary").click(function () {
+    modal(".modal", "hide");
+  });
+
+  // tabs
+  tabs(".tab-nav", ".tab-content-item", '1');
+  var demoContainer = ['.component-background', '.component-buttons', '.component-cover', '.component-dialog', '.component-flex .component-demo:first', '.component-flex .component-demo:eq(1)', '.component-flex .component-demo:eq(2)', '.component-modal', '.component-grids', '.component-table .component-demo:first', '.component-table .component-demo:eq(1)', '.component-table .component-demo:eq(2)', '.component-tabs'];
+  demoContainer.map(item => {
+    tabs(item + " .demo-nav-item", item + " .demo-content-item", '1');
+  });
 
 })
