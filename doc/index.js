bf823b5f4f0d482826d7180b2f840e9012553e54
谢德旭 <xiedx@szkingdom.com>
2020-06-19 16:04:43 +0800

    更新文档

--- doc/index.js
+++ doc/index.js
@@ -108,7 +108,7 @@ $(function () {
 
   // tabs
   tabs(".tab-nav", ".tab-content-item", '1');
-  var demoContainer = ['.component-background', '.component-buttons', '.component-cover', '.component-dialog', '.component-flex .component-demo:first', '.component-flex .component-demo:eq(1)', '.component-flex .component-demo:eq(2)', '.component-modal', '.component-grids', '.component-table .component-demo:first', '.component-table .component-demo:eq(1)', '.component-table .component-demo:eq(2)', '.component-tabs'];
+  var demoContainer = ['.component-background', '.component-buttons', '.component-cover', '.component-dialog', '.component-flex .component-demo:first', '.component-flex .component-demo:eq(1)', '.component-flex .component-demo:eq(2)', '.component-flex .component-demo:eq(3)', '.component-modal', '.component-grids', '.component-table .component-demo:first', '.component-table .component-demo:eq(1)', '.component-table .component-demo:eq(2)', '.component-tabs'];
   demoContainer.map(item => {
     tabs(item + " .demo-nav-item", item + " .demo-content-item", '1');
   });
